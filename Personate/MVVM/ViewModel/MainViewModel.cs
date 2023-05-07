48d6907979b52a7e9bea9b13a1c9cfa96430ee73
Egxr41k <egor2005krava@gmail.com>
2023-05-07 17:18:30 +0300

    starting the UWP client

--- Personate/MVVM/ViewModel/MainViewModel.cs
+++ Personate/MVVM/ViewModel/MainViewModel.cs
@@ -15,23 +15,8 @@ class MainViewModel : Base.ViewModel
             Path.Combine(
                 AppDomain.CurrentDomain.BaseDirectory,
                 @"..\..\..\Resources"));
-    #region Icons
-    public BitmapImage MinimizeIcon { get => minimizeIcon; }
-    private BitmapImage minimizeIcon;
-
-    public BitmapImage MaximizeIcon { get => maximizeIcon; }
-    private BitmapImage maximizeIcon;
-
-    public BitmapImage CloseIcon { get => closeIcon; }
-    private BitmapImage closeIcon;
-
-    #endregion
 
     #region Commands
-    public Base.Command? MaximizeCommand { get; set; }
-    public Base.Command? MinimizeCommand { get; set; }
-    public Base.Command? CloseCommand { get; set; }
-
     public Base.Command? HomeViewCommand { get; set; }
     public Base.Command? FontsViewCommand { get; set; }
     public Base.Command? ThemesViewCommand { get; set; }
@@ -57,22 +42,6 @@ class MainViewModel : Base.ViewModel
 
     #endregion
 
-    private void AppCommandsInit()
-    {
-        MinimizeCommand = new(o =>
-        {
-            MainWindow.WindowState = WindowState.Minimized;
-        });
-
-        MaximizeCommand = new(o =>
-        {
-            if(MainWindow.WindowState == WindowState.Normal) 
-                MainWindow.WindowState = WindowState.Maximized;
-            else MainWindow.WindowState = WindowState.Normal;
-        });
-
-        CloseCommand = new(o => MainWindow.Close());
-    }
     private void MenuCommandsInit()
     {
         HomeViewCommand = new(o =>
@@ -116,22 +85,13 @@ class MainViewModel : Base.ViewModel
         });
     }
 
-    private void IconsInit()
-    {
-        string IconsDirectory = RESOURCEPATH + "\\Icons";
-        string[] IconsFiles = Directory.GetFiles(IconsDirectory);
-
-        closeIcon = new BitmapImage(new Uri(IconsFiles[0]));
-        maximizeIcon = new BitmapImage(new Uri(IconsFiles[3]));
-        minimizeIcon = new BitmapImage(new Uri(IconsFiles[4]));
-    }
     public MainViewModel()
     {
 
         currentView = App.HomeVM;
 
         #region Commands init
-        AppCommandsInit();
+        
         MenuCommandsInit();
 
         WallsMenuViewModel.UploadImageCommand = new(o =>
@@ -158,9 +118,8 @@ class MainViewModel : Base.ViewModel
         {
             CurrentView = new CursorViewModel();
         });
-
         #endregion
 
-        IconsInit(); 
+
     }
 }
