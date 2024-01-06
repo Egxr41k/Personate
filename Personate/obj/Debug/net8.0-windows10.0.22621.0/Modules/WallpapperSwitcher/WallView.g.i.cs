7db47dacb78c0bf1c3199d1e292513399e3856d7
Egxr41k <egor2005krava@gmail.com>
2024-01-06 21:33:55 +0200

    changing PersonateLib/Cursor folder structure

--- Personate/obj/Debug/net8.0-windows10.0.22621.0/Modules/WallpapperSwitcher/WallView.g.i.cs
+++ Personate/obj/Debug/net8.0-windows10.0.22621.0/Modules/WallpapperSwitcher/WallView.g.i.cs
@@ -53,7 +53,7 @@ namespace Personate.Modules.WallpapperSwitcher {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Personate;V1.0.0.0;component/modules/wallpapperswitcher/wallview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Personate;component/modules/wallpapperswitcher/wallview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Modules\WallpapperSwitcher\WallView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
